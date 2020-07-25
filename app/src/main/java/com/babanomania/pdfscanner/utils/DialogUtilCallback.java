5e258157c754fe8219113a06a661c43293abbe68
Serdar Ozkan <serdarozkan41@gmail.com>
2020-07-25 18:36:36 +0300

    Initial commit

---
+++ app/src/main/java/com/babanomania/pdfscanner/utils/DialogUtilCallback.java
@@ -0,0 +1,8 @@
+package com.teesteknoloji.contractanalysis.utils;
+
+import android.content.DialogInterface;
+
+public interface DialogUtilCallback {
+
+    public void onSave( String textValue, String category );
+}
---
+++ app/src/main/java/com/babanomania/pdfscanner/utils/FileWritingCallback.java
@@ -0,0 +1,8 @@
+package com.teesteknoloji.contractanalysis.utils;
+
+import java.io.FileOutputStream;
+
+public interface FileWritingCallback {
+
+    public void write(FileOutputStream out);
+}
